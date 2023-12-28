9ce7fbe6a55bdf5ab01327ecc2690934805d77f3
yun900800@126.com <yun900800@126.com>
2023-12-28 14:03:55 +0800

    feature(ddd-spring) 学习并理解环境变量在spring中的作用

---
+++ ddd-spring/ddd-spring-env/src/test/java/org/ddd/fundamental/spring/env/ConfigurablePropertyResolverExampleTest.java
@@ -0,0 +1,12 @@
+package org.ddd.fundamental.spring.env;
+
+import org.junit.Test;
+
+public class ConfigurablePropertyResolverExampleTest {
+
+    @Test
+    public void testConfigurablePropertyResolverLoad() {
+        ConfigurablePropertyResolverExample.load();
+    }
+
+}
---
+++ ddd-spring/ddd-spring-env/src/test/java/org/ddd/fundamental/spring/env/EnvironmentExampleTest.java
@@ -0,0 +1,11 @@
+package org.ddd.fundamental.spring.env;
+
+import org.junit.Test;
+
+public class EnvironmentExampleTest {
+
+    @Test
+    public void testEnvironmentExampleLoad() {
+        EnvironmentExample.load();
+    }
+}
