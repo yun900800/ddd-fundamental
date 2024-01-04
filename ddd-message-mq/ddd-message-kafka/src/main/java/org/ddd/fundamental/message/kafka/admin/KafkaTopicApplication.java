16170647d6d50cd84c92541d8495d02c1c5f67a9
yun900800@126.com <yun900800@126.com>
2024-01-04 13:18:43 +0800

    feature(ddd-message-mq) 增加一个用于消息服务器测试的工程,主要用于测试kafka主题的创建

---
+++ ddd-message-mq/ddd-message-kafka/src/main/java/org/ddd/fundamental/message/kafka/admin/KafkaTopicApplication.java
@@ -0,0 +1,97 @@
+package org.ddd.fundamental.message.kafka.admin;
+
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.config.TopicConfig;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class KafkaTopicApplication {
+
+    private final Properties properties;
+
+    public KafkaTopicApplication(Properties properties){
+        this.properties = properties;
+    }
+
+    public void createTopic(String topicName){
+        try (Admin admin = Admin.create(properties)) {
+            int partitions = 1;
+            short replicationFactor = 1;
+            NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
+
+            CreateTopicsResult result = admin.createTopics(Collections.singleton(newTopic));
+
+            // get the async result for the new topic creation
+            KafkaFuture<Void> future = result.values()
+                    .get(topicName);
+
+            // call get() to block until topic creation has completed or failed
+            future.get();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 创建一个带有可选参数的admin
+     * @param topicName
+     * @throws Exception
+     */
+    public void createTopicWithOptions(String topicName)  {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        try (Admin admin = Admin.create(properties)) {
+            int partitions = 1;
+            short replicationFactor = 1;
+            NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
+
+            CreateTopicsOptions topicOptions = new CreateTopicsOptions().validateOnly(true)
+                    .retryOnQuotaViolation(true);
+
+            CreateTopicsResult result = admin.createTopics(Collections.singleton(newTopic), topicOptions);
+
+            KafkaFuture<Void> future = result.values()
+                    .get(topicName);
+            future.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createCompactedTopicWithCompression(String topicName) throws Exception {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        try (Admin admin = Admin.create(properties)) {
+            int partitions = 1;
+            short replicationFactor = 1;
+
+            // Create a compacted topic with 'lz4' compression codec
+            Map<String, String> newTopicConfig = new HashMap<>();
+            newTopicConfig.put(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT);
+            newTopicConfig.put(TopicConfig.COMPRESSION_TYPE_CONFIG, "lz4");
+            NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor).configs(newTopicConfig);
+
+            CreateTopicsResult result = admin.createTopics(Collections.singleton(newTopic));
+
+            KafkaFuture<Void> future = result.values()
+                    .get(topicName);
+            future.get();
+        }
+    }
+
+
+
+}
---
+++ ddd-message-mq/ddd-message-kafka/src/test/java/org/ddd/fundamental/message/kafka/admin/KafkaTopicApplicationTest.java
@@ -0,0 +1,64 @@
+package org.ddd.fundamental.message.kafka.admin;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Properties;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class KafkaTopicApplicationTest {
+
+    //创建一个kafka容器实例
+    @Container
+    private static final KafkaContainer KAFKA_CONTAINER =
+            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+
+    private KafkaTopicApplication kafkaTopicApplication;
+
+
+    @BeforeEach
+    void setup() {
+        //启动容器
+        KAFKA_CONTAINER.start();
+        //创建客户端
+        Properties properties = new Properties();
+        String bootstrap = KAFKA_CONTAINER.getBootstrapServers();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+        kafkaTopicApplication = new KafkaTopicApplication(properties);
+    }
+
+    @Test
+    void givenTopicName_whenCreateNewTopic_thenTopicIsCreated() throws Exception {
+        kafkaTopicApplication.createTopic("test-topic-with-options");
+
+        String topicCommand = "/usr/bin/kafka-topics --bootstrap-server=localhost:9092 --list";
+        String stdout = KAFKA_CONTAINER.execInContainer("/bin/sh", "-c", topicCommand)
+                .getStdout();
+        assertThat(stdout).contains("test-topic");
+    }
+
+    @Test
+    void givenTopicName_whenCreateNewTopicWithOptions_thenTopicIsCreated() throws Exception {
+        kafkaTopicApplication.createTopicWithOptions("test-topic-with-options");
+
+        String topicCommand = "/usr/bin/kafka-topics --bootstrap-server=localhost:9092 --list";
+        String stdout = KAFKA_CONTAINER.execInContainer("/bin/sh", "-c", topicCommand)
+                .getStdout();
+        assertThat(stdout).doesNotContain("test-topic-with-options");
+    }
+
+    @Test
+    void givenTopicName_whenCreateNewTopicWithCompression_thenTopicIsCreated() throws Exception {
+        kafkaTopicApplication.createCompactedTopicWithCompression("test-topic-with-compression");
+
+        String topicCommand = "/usr/bin/kafka-topics --bootstrap-server=localhost:9092 --list";
+        String stdout = KAFKA_CONTAINER.execInContainer("/bin/sh", "-c", topicCommand)
+                .getStdout();
+        assertThat(stdout).contains("test-topic-with-compression");
+    }
+}
