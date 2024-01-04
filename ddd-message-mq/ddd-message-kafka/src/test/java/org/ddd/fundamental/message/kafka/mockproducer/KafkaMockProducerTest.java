d7411baa9254f732852372a956910db93f5c9ed2
yun900800@126.com <yun900800@126.com>
2024-01-04 15:18:32 +0800

    feature(ddd-message-mq) kafka的生产者有自动flush,手动flush以及抛出异常的时候get结果也算结束

--- ddd-message-mq/ddd-message-kafka/src/test/java/org/ddd/fundamental/message/kafka/mockproducer/KafkaMockProducerTest.java
+++ ddd-message-mq/ddd-message-kafka/src/test/java/org/ddd/fundamental/message/kafka/mockproducer/KafkaMockProducerTest.java
@@ -2,12 +2,18 @@ package org.ddd.fundamental.message.kafka.mockproducer;
 
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import static java.util.Collections.emptySet;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KafkaMockProducerTest {
@@ -24,7 +30,7 @@ public class KafkaMockProducerTest {
     @Test
     void givenKeyValue_whenSend_thenVerifyHistory() throws ExecutionException, InterruptedException {
 
-        //构建自动flush的生产者
+        //构建自动flush的生产者,没有事务
         buildMockProducer(true);
         //when
         kafkaMockProducer = new KafkaMockProducer(mockProducer);
@@ -74,5 +80,47 @@ public class KafkaMockProducerTest {
             assertEquals(e, ex.getCause());
         }
         assertTrue(record.isDone());
+    }
+
+    /**
+     * 测试kafka的事务发送,没有commit之前发送成功,但是在commit之后才能看到值
+     */
+    @Test
+    void givenKeyValue_whenSendWithTxn_thenSendOnlyOnTxnCommit() {
+        //自动flush,有事务,send后没有值,一定要commit之后才有值
+        buildMockProducer(true);
+        //when
+        kafkaMockProducer = new KafkaMockProducer(mockProducer);
+        kafkaMockProducer.initTransaction();
+        kafkaMockProducer.beginTransaction();
+        Future<RecordMetadata> record = kafkaMockProducer.send("data", "{\"site\" : \"baeldung\"}");
+
+        //then
+        assertTrue(record.isDone());
+        assertTrue(mockProducer.history().isEmpty());
+        kafkaMockProducer.commitTransaction();
+        assertTrue(mockProducer.history().size() == 1);
+    }
+
+    @Test
+    void givenKeyValue_whenSendWithPartitioning_thenVerifyPartitionNumber() throws ExecutionException, InterruptedException {
+
+        PartitionInfo partitionInfo0 = new PartitionInfo(TOPIC_NAME, 0, null, null, null);
+        PartitionInfo partitionInfo1 = new PartitionInfo(TOPIC_NAME, 1, null, null, null);
+        List<PartitionInfo> list = new ArrayList<>();
+        list.add(partitionInfo0);
+        list.add(partitionInfo1);
+        Cluster cluster = new Cluster("kafka", new ArrayList<Node>(), list, emptySet(), emptySet());
+        this.mockProducer = new MockProducer<>(cluster, true, new EvenOddPartitioner(), new StringSerializer(), new StringSerializer());
+        //when
+        kafkaMockProducer = new KafkaMockProducer(mockProducer);
+        Future<RecordMetadata> recordMetadataFuture = kafkaMockProducer.send("partition", "{\"site\" : \"baeldung\"}");
+
+        Future<RecordMetadata> recordMetadataFuture1 = kafkaMockProducer.send("yun900", "{\"site1\" : \"yun900\"}");
+
+        //then
+        assertTrue(recordMetadataFuture.get().partition() == 1);
+        assertTrue(recordMetadataFuture1.get().partition() == 0);
+
     }
 }
