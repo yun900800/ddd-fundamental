9e6365747b3338d1084682aa81f9227e3ba4c5e6
yun900800@126.com <yun900800@126.com>
2024-01-10 21:02:55 +0800

    feature(ddd-spring-kafka) 增加一个测试kafka生产者和消费者功能的方法，使用嵌入式的kafka broker

--- ddd-message-mq/ddd-spring-kafka/src/test/java/org/ddd/fundamental/spring/kafka/config/KafkaTopicConfigTest.java
+++ ddd-message-mq/ddd-spring-kafka/src/test/java/org/ddd/fundamental/spring/kafka/config/KafkaTopicConfigTest.java
@@ -1,8 +1,14 @@
 package org.ddd.fundamental.spring.kafka.config;
 
+import org.ddd.fundamental.spring.kafka.KafkaApplication;
+import org.ddd.fundamental.spring.kafka.embedded.KafkaConsumer;
+import org.ddd.fundamental.spring.kafka.embedded.KafkaProducer;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +20,18 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest(classes = {KafkaApplication.class})
+@RunWith(SpringRunner.class)
 @DirtiesContext
-@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:29092", "port=29092" })
 public class KafkaTopicConfigTest {
 
     @Autowired
@@ -25,11 +39,51 @@ public class KafkaTopicConfigTest {
     @Autowired
     KafkaAdmin admin;
 
-    private String topic = "embeddedKafkaBroker";
+    @Value("${test.topic}")
+    private String topic;
+
+    @Autowired
+    public KafkaTemplate<String, String> template;
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+//    @BeforeEach
+//    public void setup() {
+//        consumer.resetLatch();
+//    }
 
     @Test
     public void testKafkaAdminNotNull(){
-        Assert.assertNull(admin);
+
+        Assert.assertNotNull(admin);
+    }
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingWithDefaultTemplate_thenMessageReceived() throws Exception {
+        String data = "Sending with default template";
+
+        template.send(topic, data);
+
+        boolean messageConsumed = consumer.getLatch()
+                .await(10, TimeUnit.SECONDS);
+        assertTrue(messageConsumed);
+        assertThat(consumer.getPayload(), containsString(data));
+    }
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived() throws Exception {
+        String data = "Sending with our own simple KafkaProducer";
+
+        producer.send(topic, data);
+
+        boolean messageConsumed = consumer.getLatch()
+                .await(10, TimeUnit.SECONDS);
+        assertTrue(messageConsumed);
+        assertThat(consumer.getPayload(), containsString(data));
     }
 
     @TestConfiguration
