26a454216aedbef49afb46ae27cb79798f4708b7
yun900800@126.com <yun900800@126.com>
2024-01-11 20:39:21 +0800

    feature(ddd-shared) 增加一个测试聚合中事件的类

--- ddd-share/src/test/java/org/ddd/fundamental/share/domain/AggregateRootTest.java
+++ ddd-share/src/test/java/org/ddd/fundamental/share/domain/AggregateRootTest.java
@@ -20,8 +20,30 @@ public class AggregateRootTest {
         // 注意这里测试的时候pullDomainEvents只能调用一次
         List<DomainEvent> events = absCls.pullDomainEvents();
         Assert.assertEquals(events.size(),1);
-        Assert.assertEquals(1,events.size());
         Assert.assertEquals(event,events.get(0));
+        events = absCls.pullDomainEvents();
+        Assert.assertEquals(events.size(),0);
     }
+
+    @Test
+    public void testMultipleRecordAndPullDomainEvents(){
+        AggregateRoot absCls = Mockito.spy(
+                AggregateRoot.class);
+        DomainEvent event = Mockito.mock(
+                DomainEvent.class,
+                Mockito.CALLS_REAL_METHODS);
+        DomainEvent event1 = Mockito.mock(
+                DomainEvent.class,
+                Mockito.CALLS_REAL_METHODS);
+        absCls.record(event);
+        absCls.record(event1);
+        absCls.record(event1);
+        List<DomainEvent> events = absCls.pullDomainEvents();
+        Assert.assertEquals(events.size(),3);
+        Assert.assertEquals(event1,events.get(1));
+        events = absCls.pullDomainEvents();
+        Assert.assertEquals(events.size(),0);
+    }
+
 
 }
