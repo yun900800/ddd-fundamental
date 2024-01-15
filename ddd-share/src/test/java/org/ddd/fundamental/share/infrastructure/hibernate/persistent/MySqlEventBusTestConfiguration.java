affaaf54ee05b5cc1e5224d5c93a8f0c31cfc4d6
yun900800@126.com <yun900800@126.com>
2024-01-15 11:58:24 +0800

    feature(ddd-shared) 增加使用test-container-mysql使用的方式

--- ddd-share/src/test/java/org/ddd/fundamental/share/infrastructure/hibernate/persistent/MySqlEventBusTestConfiguration.java
+++ ddd-share/src/test/java/org/ddd/fundamental/share/infrastructure/hibernate/persistent/MySqlEventBusTestConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.testcontainers.containers.MySQLContainer;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -16,7 +17,17 @@ import java.io.IOException;
 @TestConfiguration
 public class MySqlEventBusTestConfiguration {
 
-    public static CustomMySqlContainer mySqlContainer = CustomMySqlContainer.getInstance();
+//    public CustomMySqlContainer mySqlContainer = CustomMySqlContainer.getInstance();
+//    {
+//        mySqlContainer.start();
+//    }
+
+    static final MySQLContainer MY_SQL_CONTAINER;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+        MY_SQL_CONTAINER.start();
+    }
     private final String                        CONTEXT_NAME = "fundamental";
 
     @Autowired
@@ -27,13 +38,11 @@ public class MySqlEventBusTestConfiguration {
     }
 
     @Bean("fundamental-transaction_manager")
-    @ConditionalOnMissingBean
     public PlatformTransactionManager hibernateTransactionManager() throws IOException, ParameterNotExist {
         return factory.hibernateTransactionManager(sessionFactory());
     }
 
     @Bean("fundamental-session_factory")
-    @ConditionalOnMissingBean
     public LocalSessionFactoryBean sessionFactory() throws IOException, ParameterNotExist {
 
         DataSource newDataSource = factory.proxyDataSource(dataSource());
@@ -41,14 +50,14 @@ public class MySqlEventBusTestConfiguration {
     }
 
     @Bean("fundamental-data_source")
-    @ConditionalOnMissingBean
     public DataSource dataSource() throws IOException, ParameterNotExist {
+        System.out.println("init dataSource mysql-container in test Environment");
         return factory.dataSource(
-                mySqlContainer.getHost(),
-                3306,
-                mySqlContainer.getDatabaseName(),
-                mySqlContainer.getUsername(),
-                mySqlContainer.getPassword()
+                MY_SQL_CONTAINER.getHost(),
+                MY_SQL_CONTAINER.getFirstMappedPort(),
+                MY_SQL_CONTAINER.getDatabaseName(),
+                MY_SQL_CONTAINER.getUsername(),
+                MY_SQL_CONTAINER.getPassword()
         );
     }
 }
