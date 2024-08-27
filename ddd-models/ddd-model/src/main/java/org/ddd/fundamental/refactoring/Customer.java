ff96e78623109d699cfc0dccb9ed5383f3fcc304
yun900800@126.com <yun900800@126.com>
2024-08-27 14:27:17 +0800

    feature(ddd-models) 修改amountFor方法中的命名,版本V1.0.2;重構技巧是method rename

--- ddd-models/ddd-model/src/main/java/org/ddd/fundamental/refactoring/Customer.java
+++ ddd-models/ddd-model/src/main/java/org/ddd/fundamental/refactoring/Customer.java
@@ -22,25 +22,25 @@ public class Customer {
         return name;
     }
 
-    private double amountFor(Rental rental){
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()){
+    private double amountFor(Rental aRental){
+        double result = 0;
+        switch (aRental.getMovie().getPriceCode()){
             case Movie.REGULAR:
-                thisAmount +=2;
-                if (rental.getDaysRented() > 2)
-                    thisAmount += (rental.getDaysRented()-2)*1.5;
+                result +=2;
+                if (aRental.getDaysRented() > 2)
+                    result += (aRental.getDaysRented()-2)*1.5;
 
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount +=rental.getDaysRented()*3;
+                result +=aRental.getDaysRented()*3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount+=1.5;
-                if (rental.getDaysRented() >1)
-                    thisAmount += (rental.getDaysRented()-3)*1.5;
+                result+=1.5;
+                if (aRental.getDaysRented() >1)
+                    result += (aRental.getDaysRented()-3)*1.5;
                 break;
         }
-        return thisAmount;
+        return result;
     }
 
 
