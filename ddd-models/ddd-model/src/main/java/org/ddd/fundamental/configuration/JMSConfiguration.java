44f59ce63e98040109c348c946f16181ffa99aa0
yun900800@126.com <yun900800@126.com>
2024-09-03 16:20:49 +0800

    feature(ddd-models) 增加jms的配置和数据库的配置

---
+++ ddd-models/ddd-model/src/main/java/org/ddd/fundamental/configuration/JMSConfiguration.java
@@ -0,0 +1,39 @@
+package org.ddd.fundamental.configuration;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQXAConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jta.atomikos.AtomikosConnectionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+@Configuration
+@EnableJms
+public class JMSConfiguration {
+
+    @Value("${jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public JmsTemplate jmsTemplate() throws Throwable {
+        return new JmsTemplate(connectionFactory());
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public ConnectionFactory connectionFactory() throws JMSException {
+        ActiveMQXAConnectionFactory activeMQXAConnectionFactory = new
+                ActiveMQXAConnectionFactory();
+        activeMQXAConnectionFactory.setBrokerURL(jmsUrl);
+        AtomikosConnectionFactoryBean atomikosConnectionFactoryBean = new AtomikosConnectionFactoryBean();
+        atomikosConnectionFactoryBean.setUniqueResourceName("xamq");
+        atomikosConnectionFactoryBean.setLocalTransactionMode(false);
+        atomikosConnectionFactoryBean.setXaConnectionFactory(activeMQXAConnectionFactory);
+        return atomikosConnectionFactoryBean;
+    }
+
+
+}
