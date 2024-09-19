52a59f9f0ddb799c0587e70127c38e92c03d4ae1
yun900800@126.com <yun900800@126.com>
2024-09-19 14:20:44 +0800

    feature(ddd-models) 重构了代码, 将合并的方法提取到抽象类中

--- ddd-models/ddd-model/src/main/java/org/ddd/fundamental/domain/order/impl/PurchaseOrder.java
+++ ddd-models/ddd-model/src/main/java/org/ddd/fundamental/domain/order/impl/PurchaseOrder.java
@@ -76,63 +76,15 @@ public class PurchaseOrder implements IOrder<String,IItem<String>> {
      */
     @Override
     public List<IItem<String>> mergeItems() {
-        List<IItem<String>> mergeItems = new ArrayList<>();
-        Map<ItemType,List<IItem<String>>> itemMap = items.stream()
-                .collect(Collectors.groupingBy(IItem::type));
-        mergeItems.add(mergeListToItem(itemMap,ItemType.RAW_MATERIAL));
-        mergeItems.add(mergeListToItem(itemMap,ItemType.WORK_IN_PROGRESS));
-        mergeItems.add(mergeListToItem(itemMap,ItemType.FINISHED_PRODUCT));
-        return mergeItems;
+        return Item.mergeItems(items);
     }
 
     @Override
     public List<IItem<String>> mergeItemsByKey() {
-        List<IItem<String>> mergeItems = new ArrayList<>();
-        Map<String,List<IItem<String>>> itemMap = items.stream()
-                .collect(Collectors.groupingBy(IItem::key));
-        for (String key: keys()){
-            mergeItems.add(mergeListToItemByKey(itemMap,key));
-        }
-        return mergeItems;
+        return Item.mergeItemsByKey(items, keys());
     }
 
-    private IItem<String> mergeListToItemByKey(Map<String,List<IItem<String>>> itemMap,String key) {
-        List<IItem<String>> materialItems = itemMap.get(key);
-        if (CollectionUtils.isEmpty(materialItems)) {
-            return null;
-        }
-        IItem<String> item0 = materialItems.get(0);
-        double sumQuantity = 0.0;
-        for (IItem<String> item: materialItems) {
-            sumQuantity+=item.quantity();
-        }
-        return Item.copy(item0).changeQuantity(sumQuantity);
-    }
 
-    private static IItem<String> createItem(ItemType type,IItem<String> item, double quantity){
-        IItem<String> itemNew = null;
-        if (ItemType.RAW_MATERIAL == type) {
-            itemNew = new RawMaterial(item.name(),quantity);
-        } else if (ItemType.WORK_IN_PROGRESS == type) {
-            itemNew = new WorkInProgress(item.name(),quantity);
-        } else if (ItemType.FINISHED_PRODUCT == type) {
-            itemNew = new FinishedProduct(item.name(),quantity);
-        }
-        return itemNew;
-    }
-
-    private IItem<String> mergeListToItem(Map<ItemType,List<IItem<String>>> itemMap,ItemType type) {
-        List<IItem<String>> materialItems = itemMap.get(type);
-        if (CollectionUtils.isEmpty(materialItems)) {
-            return null;
-        }
-        IItem<String> item0 = materialItems.get(0);
-        double sumQuantity = 0.0;
-        for (IItem<String> item: materialItems) {
-            sumQuantity+=item.quantity();
-        }
-        return createItem(type,item0,sumQuantity);
-    }
 
     @Override
     public boolean validOrder() {
