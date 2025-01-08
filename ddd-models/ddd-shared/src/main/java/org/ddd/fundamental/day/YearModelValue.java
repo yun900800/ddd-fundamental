29a575b7c4192aa94ecb0d43d9b69eece7063aab
yun900800@126.com <yun900800@126.com>
2025-01-08 19:58:01 +0800

    [ddd-shared] 去掉不需要的代码

--- ddd-models/ddd-shared/src/main/java/org/ddd/fundamental/day/YearModelValue.java
+++ ddd-models/ddd-shared/src/main/java/org/ddd/fundamental/day/YearModelValue.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.ddd.fundamental.core.ValueObject;
 
 import javax.persistence.*;
-import javax.validation.OverridesAttribute;
 import java.time.LocalTime;
 import java.util.Objects;
 
